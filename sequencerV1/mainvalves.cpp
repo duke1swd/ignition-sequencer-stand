b294108bd0771702ef37f713e81574fa7120ab28
Stephen Daniel <swd@pobox.com>
2018-01-24 10:04:35 -0500

    Undo last commit. Daq lines were already being toggled.

--- sequencerV1/mainvalves.cpp
+++ sequencerV1/mainvalves.cpp
@@ -143,8 +143,6 @@ void mainValveTestExit()
 {
 	mainN2OClose();
 	mainIPAClose();
-	o_daq0->cur_state = off;
-	o_daq1->cur_state = off;
 	valveTestMode = false;
 }
 
@@ -154,8 +152,6 @@ void mainValveTestExit()
  */
 void mainValveTestEnter()
 {
-	o_daq0->cur_state = off;
-	o_daq1->cur_state = off;
 	tft.fillScreen(TM_TXT_BKG_COLOR);
 	tft.setTextSize(TM_TXT_SIZE+1);
 	tft.setCursor(2, TM_TXT_OFFSET);
@@ -190,19 +186,14 @@ const struct state * mainValveTestCheck()
 	if (safe_ok()) {
 		ls1 = i_push_1->current_val;
 		ls2 = i_push_2->current_val;
-		if (ls1 && !ols1) {
-			o_daq0->cur_state = on;
+		if (ls1 && !ols1)
 			mainIPAOpen();
-		} if (ls2 && !ols2) {
-			o_daq1->cur_state = on;
+		if (ls2 && !ols2)
 			mainN2OOpen();
-		} if (!ls1 && ols1) {
-			o_daq0->cur_state = off;
+		if (!ls1 && ols1)
 			mainIPAClose();
-		} if (!ls2 && ols2) {
-			o_daq1->cur_state = off;
+		if (!ls2 && ols2)
 			mainN2OClose();
-		}
 	}
 	mainButtonDisplay();
 
